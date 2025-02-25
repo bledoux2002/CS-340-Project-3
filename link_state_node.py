9f064b5b3f938027e2ec515d4c601fc7911d17b6
Benjamin Ledoux <bledoux2002@gmail.com>
2025-02-25 13:00:48 -0600

    added descriptions for functions

--- link_state_node.py
+++ link_state_node.py
@@ -7,17 +7,74 @@ class Link_State_Node(Node):
 
     # Return a string
     def __str__(self):
+        """
+        Prints represenation of node state for debugging purposes,
+        not strictly necessary to implement but helpful
+        
+        Parameters:
+        None
+
+        Returns:
+        state (str): representation of node state
+        
+        """
         return "Rewrite this function to define your node dump printout"
 
-    # Fill in this function
+    # Called to inform Node that outgoing link properties have changed
     def link_has_been_updated(self, neighbor, latency):
+        """
+        Updates node tables with new latency to reach neighbor,
+        and forwards this update to node's neighbors.
+
+        Parameters:
+        neighbor (Node): neighbor sending the update
+        latency (float): new latency to reach neighbor
+
+        Returns:
+        None
+        """
+        # step 1: Update Tables w/ new latency to reach neighbor
         # latency = -1 if delete a link
+
+
+        # step 2: Update neighbors with new latency to neighbor
+        
+        
         pass
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
+        """
+        Choose course of action for message, sending message to
+        neighbor(s) and/or updating tables
+
+        Parameters:
+        m (str): routing message
+
+        Returns:
+        None
+        
+        """
         pass
+
+        # step 1: interpret message
+
+
+        # step 2: choose course of action
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        """
+        Node is asked which hop it THINKS is next on path to destination
+        
+        Parameters:
+        destination (Node): final destination being searched for
+        
+        Returns:
+        hops (int): next Node to reach destination
+        
+        """
+        # step 1: determine next node to destination from table?
+
+        hops = -1
+        return hops
